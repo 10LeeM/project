1d87b16394bad9bd4c5b859b21cec047f02d37de
suhade <44269319+suhade@users.noreply.github.com>
2021-06-14 11:57:21 +0300

    doctor

--- src/pages/doctor/DoctorAppointment.tsx
+++ src/pages/doctor/DoctorAppointment.tsx
@@ -1,11 +1,83 @@
-import React from 'react'
 
-function DoctorAppointment() {
-    return (
-        <div>
-            
-        </div>
-    )
+import React from 'react';
+import { withStyles, Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }),
+)(TableCell);
+
+const StyledTableRow = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  }),
+)(TableRow);
+
+function createData(name: string, email: string, phone: number, location: string, protein: number) {
+  return { name, email, phone, location, protein };
 }
 
-export default DoctorAppointment
+const rows = [
+  createData('Frozen yoghurt', 'gmail@gmail.com', 6.0, 'Kiambu', 4.0),
+  createData('Ice cream sandwic', 'gmail@gmail.com', 9.0, 'Kiambu', 4.3),
+  createData('Eclair', 'gmail@gmail.com', 16.0, 'Kiambu', 6.0),
+  createData('Cupcake', 'gmail@gmail.com', 3.7, 'Kiambu', 4.3),
+  createData('Gingerbread', 'gmail@gmail.com', 16.0, 'Kiambu', 3.9),
+];
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
+
+export default function DoctorAppointment() {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Patient</StyledTableCell>
+            <StyledTableCell align="right">Email</StyledTableCell>
+            <StyledTableCell align="right">Phone</StyledTableCell>
+            <StyledTableCell align="right">Location</StyledTableCell>
+            <StyledTableCell align="right">Time</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.email}</StyledTableCell>
+              <StyledTableCell align="right">{row.phone}</StyledTableCell>
+              <StyledTableCell align="right">{row.location}</StyledTableCell>
+              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- src/pages/doctor/DoctorOrder.tsx
+++ src/pages/doctor/DoctorOrder.tsx
@@ -1,11 +1,132 @@
-import React from 'react'
 
-function DoctorOrder() {
-    return (
-        <div>
-            
-        </div>
-    )
+
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import { useQuery } from "react-query";
+import { sendReq } from "../../api/api";
+import { getUserProfile } from "../../store/slices/userSlice";
+import { useSelector } from "react-redux";
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
+function createData(
+  product: string,
+  quantity: number,
+  customer: string,
+  contact: number,
+  price: number
+) {
+  return { product, quantity, customer, contact, price };
 }
 
-export default DoctorOrder
+const rows = [
+  createData("Frozen yoghurt", 159, "Jane", 24, 4.0),
+  createData("Ice cream sandwich", 237, "Jane", 37, 4.3),
+  createData("Eclair", 262, "Jane", 24, 6.0),
+];
+
+export default function DoctorOrder() {
+  const classes = useStyles();
+
+  const user = useSelector(getUserProfile);
+
+  const [orders, setOrders] = useState<any[]>([]);
+
+  const { isLoading } = useQuery(
+    "Get Patient's order",
+    async () => {
+      return await sendReq({
+        endpoint: `/patients/${user?.id}/orders`,
+        method: "GET",
+        auth: true,
+      });
+    },
+    {
+      onSuccess(data: any[]) {
+        // console.log(data);
+        setOrders(data);
+      },
+      enabled: user?.id !== undefined,
+    }
+  );
+
+  console.log(isLoading, orders);
+
+  return (
+    <div>
+      {" "}
+      <div>
+        <TableContainer component={Paper}>
+          <h2>Patients In Progress</h2>
+          <Table className={classes.table} aria-label="caption table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Product</TableCell>
+                <TableCell align="right">Quantity</TableCell>
+                <TableCell align="right">Customer</TableCell>
+                <TableCell align="right">Contact</TableCell>
+                <TableCell align="right">Price</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.product}>
+                  <TableCell component="th" scope="row">
+                    {row.product}
+                  </TableCell>
+                  <TableCell align="right">{row.quantity}</TableCell>
+                  <TableCell align="right">{row.customer}</TableCell>
+                  <TableCell align="right">{row.contact}</TableCell>
+                  <TableCell align="right">{row.price}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+      <br />
+      <br />
+      <div>
+        <TableContainer component={Paper}>
+          <h2>Completed Cases</h2>
+          <Table className={classes.table} aria-label="caption table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Product</TableCell>
+                <TableCell align="right">Quantity</TableCell>
+                <TableCell align="right">Customer</TableCell>
+                <TableCell align="right">Contact</TableCell>
+                <TableCell align="right">Price</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.product}>
+                  <TableCell component="th" scope="row">
+                    {row.product}
+                  </TableCell>
+                  <TableCell align="right">{row.quantity}</TableCell>
+                  <TableCell align="right">{row.customer}</TableCell>
+                  <TableCell align="right">{row.contact}</TableCell>
+                  <TableCell align="right">{row.price}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  );
+}
+
